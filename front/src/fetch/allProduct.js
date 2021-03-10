e478ace19b2399ce9e36df77ba5d3fba668004b4
QuentinFournival <quentinfournival2@gmail.com>
2021-03-10 16:30:20 +0100

    fetch

--- front/src/fetch/allProduct.js
+++ front/src/fetch/allProduct.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:3001/find/All/Produit"
+const URL = "http://localhost:3001/All/Produit"
 const getProduct = () => {
 
 const params = {
@@ -12,7 +12,36 @@ const params = {
 }
 fetch(URL,params).then(res => {
     res.json().then(json =>{
-        console.log(json)
+        console.log(json);
+            document.getElementById('container-carte').innerHTML=json.TitreAnnonce;
+            console.log(json.Message.length)
+            for(let h=0;h<json.Message.length;h++){
+                const ann=document.getElementsByClassName('carte');
+                const span=document.createElement('span');
+                const image=document.createElement('img');
+                const nom=document.createElement('h1');
+                const description = document.createElement('p');
+                const prix = document.createElement('p');
+                ann.appendChild(carte);
+                ann.appendChild(span);
+                ann.appendChild(image);
+                ann.appendChild(nom);
+                ann.appendChild(description);
+                ann.appendChild(prix);
+                const idann=document.getElementById('idAnn');
+
+                document.getElementById('Receveur').value=json.receveur;
+
+                document.getElementById('envoyeur').value=json.envoyeur;
+                idann.value=json.IdAnn;
+
+                nom.innerHTML=json.Message[h].Nom;
+                description.innerHTML=json.Message[h].Description;
+                prix.innerHTML=json.Message[h].Prix
+                
+
+            }
+
     })
 })
 }
--- front/src/pages/produit.tsx
+++ front/src/pages/produit.tsx
@@ -4,15 +4,12 @@ import {Link} from 'react-router-dom';
 import banane from '../imgs/banane.png';
 import pomme from '../imgs/pomme.png';
 import riz from '../imgs/riz.png';
-import getProduct from '../fetch/allProduct'
 import '../css/styles.css';
 import '../css/reset.css';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 const Produit: FunctionComponent = () => {
  useEffect(() => {
-    getProduct()
-
   }, []);
 
   return (
@@ -43,76 +40,11 @@ const Produit: FunctionComponent = () => {
                 <Menu.Item key="5">Produit Frais</Menu.Item>
                 <Menu.Item key="6">Sans Gluten</Menu.Item>
                 <Menu.Item key="7">Vegan</Menu.Item>
-            {/* <SubMenu key="sub1"  title="fruit & legumes"> */}
-
-              {/* <Menu.Item key="2">origine</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item> */}
-            {/* </SubMenu> */}
-            {/* <SubMenu key="sub2"  title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3"  title="subnav 3">
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu> */}
+       
           </Menu>
         </Sider>
-        <Content className="top" style={{ padding: '0 24px', minHeight: 280 }}><div className="container-carte">
-          <div className="carte">
-              <span></span>
-              <img src={pomme} alt=""/>
-              <h1>Pomme</h1>
-              <p></p>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={banane} alt=""/>
-              <h1>Banane</h1>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={riz} alt=""/>
-              <h1>Riz</h1>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={pomme} alt=""/>
-              <h1>Pomme</h1>
-              <p></p>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={banane} alt=""/>
-              <h1>Banane</h1>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={riz} alt=""/>
-              <h1>Riz</h1>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={pomme} alt=""/>
-              <h1>Pomme</h1>
-              <p></p>
-
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={banane} alt=""/>
-              <h1>Banane</h1>
-          </div>
-          <div className="carte">
-              <span></span>
-              <img src={riz} alt=""/>
-              <h1>Riz</h1>
-          </div>
+        <Content className="top" style={{ padding: '0 24px', minHeight: 280 }}><div id="container-carte" className="container-carte">
+         
       </div></Content>
       </Layout>
     </Content>

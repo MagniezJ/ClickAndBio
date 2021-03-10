814e00f10627cbbe17b6a4fab38dcbadff16e379
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2021-03-10 13:33:34 +0100

    cors en place

--- back/app/app.ts
+++ back/app/app.ts
@@ -2,7 +2,7 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import  Road from "../route/road";
 import fs = require('fs');
-
+import * as cors from "cors"
 class App {
 
     public express: express.Application;
@@ -16,13 +16,28 @@ class App {
 
     // Configure Express middleware.
     private middleware(): void {
+        var corsOptions={ //options d autorisation d acces
+  origin:"http://localhost:3000" //URL front 
+}
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use(function(req, res, next) { //autorisation pour fetch
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+            res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+          });
+          this.express.use(function (err, req, res, next) {
+            console.log('This is the invalid field ->', err.field)
+            next(err)
+          })
+          this.express.use(cors())
+          this.express.use(cors(corsOptions))
     }
     routes():void{
         this.express.use('/', Road)
     }
-   
+ 
 }
 
 export default new App().express;
